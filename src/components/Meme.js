a371dd496595fa1606c112f8af851be32590ae99
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2022-07-24 20:33:22 +0530

    added image adder functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,22 +2,29 @@ import React from 'react'
 import memesData from '../memesData';
 
 function Meme() {
+    let [memeImage, setMemeImage] = React.useState('');
+
     function getMemeImage() {
-        const memesArray = memesData.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
-        console.log(memesArray[randomNumber].url);
+        let len = memesData.data.memes.length;
+        let random = Math.floor(Math.random() * len);
+        let randomImageUrl = memesData.data.memes[random].url;
+        setMemeImage(randomImageUrl);
     }
-  return (
-    <main>
-        <div className='meme'>
-            <div className='inputs'>
-                <input type='text' placeholder='Top Text'></input>
-                <input type='text' placeholder='Bottom Text'></input>
+
+    return (
+        <main>
+            <div className='meme'>
+                <div className='inputs'>
+                    <input type='text' placeholder='Top Text'></input>
+                    <input type='text' placeholder='Bottom Text'></input>
+                </div>
+                <button className='new-img-btn' onClick={getMemeImage} >Get a new meme image 🖼️</button>
+                <div className='meme-main'>
+                    <img src = {memeImage} />
+                </div>
             </div>
-            <button className='new-img-btn' onClick={getMemeImage} >Get a new meme image 🖼️</button>
-        </div>
-    </main>
-  )
+        </main>
+    )
 }
 
 export default Meme
